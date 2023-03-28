b24c2a171499e27c082fbdba61eb396aeae11773
DoHKapJleoHe <91648774+DoHKapJleoHe@users.noreply.github.com>
2023-03-29 01:37:58 +0700

    Created Spring project

---
+++ src/main/java/ru/nsu/fit/g20202/scimservice/ScimServiceApplication.java
@@ -0,0 +1,15 @@
+package ru.nsu.fit.g20202.scimservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScimServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ScimServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nsu/fit/g20202/scimservice/ScimServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.g20202.scimservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScimServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
