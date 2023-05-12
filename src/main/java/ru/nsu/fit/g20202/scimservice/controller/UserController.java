6bc00c220804a62a566575f0e9b0e84f3e707d74
vkharchenko1 <91654580+vkharchenko1@users.noreply.github.com>
2023-05-12 22:14:20 +0700

    Changed return types to ResponseBody and added HTTP status to responses.

--- src/main/java/ru/nsu/fit/g20202/scimservice/controller/UserController.java
+++ src/main/java/ru/nsu/fit/g20202/scimservice/controller/UserController.java
@@ -2,15 +2,15 @@ package ru.nsu.fit.g20202.scimservice.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.nsu.fit.g20202.scimservice.dto.UserDTO;
 import ru.nsu.fit.g20202.scimservice.entity.User;
 import ru.nsu.fit.g20202.scimservice.mappers.UserMapper;
-import ru.nsu.fit.g20202.scimservice.repository.UserRepository;
 import ru.nsu.fit.g20202.scimservice.service.UserService;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/User")
@@ -20,40 +20,50 @@ public class UserController
     private UserService userService;
 
     @GetMapping
-    public List<UserDTO> getAllUsers()
+    public ResponseEntity<List<UserDTO>> getAllUsers()
     {
-        var users =  userService.getAllUsers();
+        List<User> users =  userService.getAllUsers();
 
-        return users.stream().map(UserMapper::toDTO).toList();
+        return new ResponseEntity<>(users.stream().map(UserMapper::toDTO).toList(), HttpStatus.OK);
     }
 
     @PostMapping
-    public void addUser(@RequestBody UserDTO newUserDTO) {
+    public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO newUserDTO)
+    {
         User newUser = UserMapper.toEntity(newUserDTO);
 
-        userService.createUser(newUser);
-    }
+        User responseUser = userService.createUser(newUser);
+
+        UserDTO responseUserDTO = UserMapper.toDTO(responseUser);
+
+        return new ResponseEntity<>(responseUserDTO, HttpStatus.CREATED);
+}
 
     @GetMapping("/{id}")
-    public UserDTO getUserById(@PathVariable Integer id)
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Integer id)
     {
-        Optional<User> user = userService.getUserById(id);
+        User user = userService.getUserById(id);
 
-        // TODO: Should notify that no user was found
-        return user.map(UserMapper::toDTO).orElse(null);
+        return new ResponseEntity<>(UserMapper.toDTO(user), HttpStatus.OK);
     }
 
 
     @PutMapping("/{id}")
-    public void replaceUserById(@RequestBody UserDTO newUserDTO, @PathVariable Integer id)
+    public ResponseEntity<UserDTO> replaceUserById(@RequestBody UserDTO newUserDTO, @PathVariable Integer id)
     {
         User newUser = UserMapper.toEntity(newUserDTO);
 
-        userService.replaceUserById(newUser, id);
+        User responseUser = userService.replaceUserById(newUser, id);
+
+        UserDTO responseUserDTO = UserMapper.toDTO(responseUser);
+
+        return new ResponseEntity<>(responseUserDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Integer id) {
+    public ResponseEntity<Object> deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
