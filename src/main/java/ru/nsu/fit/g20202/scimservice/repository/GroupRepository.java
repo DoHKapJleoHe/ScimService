74b56ec26be1df3afc559be2ad2f860163229755
DoHKapJleoHe <91648774+DoHKapJleoHe@users.noreply.github.com>
2023-05-19 23:21:03 +0700

    Added method to user repo and group repo

--- src/main/java/ru/nsu/fit/g20202/scimservice/repository/GroupRepository.java
+++ src/main/java/ru/nsu/fit/g20202/scimservice/repository/GroupRepository.java
@@ -1,8 +1,10 @@
 package ru.nsu.fit.g20202.scimservice.repository;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nsu.fit.g20202.scimservice.entity.Group;
 
 public interface GroupRepository extends JpaRepository<Group, Integer>
 {
+    boolean existsByDisplayName(@NotBlank String displayName);
 }
