7e2a8aa9ba5d1b4bc18e2dabab7def10fe353d2e
vkharchenko1 <91654580+vkharchenko1@users.noreply.github.com>
2023-06-03 09:57:54 +0700

    Create test for posting User

--- src/test/java/ru/nsu/fit/g20202/scimservice/ScimServiceApplicationTests.java
+++ src/test/java/ru/nsu/fit/g20202/scimservice/ScimServiceApplicationTests.java
@@ -1,13 +1,67 @@
 package ru.nsu.fit.g20202.scimservice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.nsu.fit.g20202.scimservice.controller.UserController;
+import ru.nsu.fit.g20202.scimservice.dto.UserDTO;
+import ru.nsu.fit.g20202.scimservice.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.nsu.fit.g20202.scimservice.repository.UserRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class ScimServiceApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void givenUserDTO_whenCreateUser_thenReturnSavedUser() throws Exception {
+        // given - precondition or setup
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserName("Renna");
+
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/Users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDTO)));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userName",
+                        is(userDTO.getUserName())));
     }
 
 }
